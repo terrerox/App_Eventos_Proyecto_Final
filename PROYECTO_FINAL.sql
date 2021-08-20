cb3cefddacd7baed47f9a180ac61b52c2338ca69
Edwin Roman <romanseberino@gmail.com>
2021-08-20 09:13:24 -0400

    He añadido 5 registros para poder tener data que mostrar en el JTable y para comprobar que todo ande bien.

--- PROYECTO_FINAL.sql
+++ PROYECTO_FINAL.sql
@@ -11,3 +11,13 @@ CREATE TABLE EVENTOS (
 	DETALLES text NOT NULL,
 	PRIMARY KEY(ID)
 );
+
+INSERT INTO EVENTOS
+VALUES
+(NULL,'English Test','15:00','18:30','ITLA Language School - 105','2021-08-20','Take the english test intermediate TOEFL.'),
+(NULL,'Math Work','20:00','20:30','Mi casa','2021-08-21','Terminal el trabajo final.'),
+(NULL,'Hacer un cambio linea 09','01:00','01:50','Soto-tech','2021-08-22','Cambios del proyecto Soto_LAN'),
+(NULL,'Sleep','21:00','06:30','Descansar','2021-08-23','Dormir PILA!');
+
+SELECT *
+FROM EVENTOS;
