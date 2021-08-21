f9fa517cd37289e21be3dbdb4ecb33b9dd273b70
Edwin <67524129+Edwinroman30@users.noreply.github.com>
2021-08-20 21:00:59 -0400

    Consultas de filtrado
    Estas son algunas consultas de filtrados, las cuales serán útiles para el segundo requerimiento del proyecto final.

--- PROYECTO_FINAL.sql
+++ PROYECTO_FINAL.sql
@@ -21,3 +21,25 @@ VALUES
 
 SELECT *
 FROM EVENTOS;
+
+/**-----FILTROS ESPECIALES-----**/
+
+SELECT * 
+FROM eventos
+order by FECHA DESC;
+
+SELECT * 
+FROM eventos
+order by HORA_INICIO DESC;
+
+SELECT * 
+FROM eventos
+order by HORA_FINAL DESC;
+
+SELECT * 
+FROM eventos
+WHERE DETALLES LIKE '%TE%'; /*PARCEO DESDE LA APP*/
+
+SELECT * 
+FROM eventos
+WHERE NOMBRE LIKE '%mat%'; /*PARCEO DESDE LA APP*/
